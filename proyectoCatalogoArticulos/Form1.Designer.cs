36a0bb13d0ed2cc85a142d9aca85ffc7b746f155
Alejandro <alejandroeaguilera0@gmail.com>
2025-04-11 14:21:37 -0300

    crear disenio inicial formulario principal

---
+++ proyectoCatalogoArticulos/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace proyectoCatalogoArticulos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnDetalles = new System.Windows.Forms.Button();
+            this.btnListarCategoria = new System.Windows.Forms.Button();
+            this.btnListarMarca = new System.Windows.Forms.Button();
+            this.btnFiltrar = new System.Windows.Forms.Button();
+            this.txtFiltro = new System.Windows.Forms.TextBox();
+            this.cbFiltro = new System.Windows.Forms.ComboBox();
+            this.pbArticulo = new System.Windows.Forms.PictureBox();
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pbArticulo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAgregar.Location = new System.Drawing.Point(21, 399);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(95, 39);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnModificar.Location = new System.Drawing.Point(122, 399);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(95, 39);
+            this.btnModificar.TabIndex = 1;
+            this.btnModificar.Text = "MODIFICAR";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnEliminar.Location = new System.Drawing.Point(223, 399);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(95, 39);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnDetalles
+            // 
+            this.btnDetalles.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnDetalles.Location = new System.Drawing.Point(324, 399);
+            this.btnDetalles.Name = "btnDetalles";
+            this.btnDetalles.Size = new System.Drawing.Size(95, 39);
+            this.btnDetalles.TabIndex = 3;
+            this.btnDetalles.Text = "DETALLES";
+            this.btnDetalles.UseVisualStyleBackColor = true;
+            // 
+            // btnListarCategoria
+            // 
+            this.btnListarCategoria.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnListarCategoria.Location = new System.Drawing.Point(577, 399);
+            this.btnListarCategoria.Name = "btnListarCategoria";
+            this.btnListarCategoria.Size = new System.Drawing.Size(95, 39);
+            this.btnListarCategoria.TabIndex = 4;
+            this.btnListarCategoria.Text = "LISTAR CATEGORIA";
+            this.btnListarCategoria.UseVisualStyleBackColor = true;
+            // 
+            // btnListarMarca
+            // 
+            this.btnListarMarca.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnListarMarca.Location = new System.Drawing.Point(678, 399);
+            this.btnListarMarca.Name = "btnListarMarca";
+            this.btnListarMarca.Size = new System.Drawing.Size(95, 39);
+            this.btnListarMarca.TabIndex = 5;
+            this.btnListarMarca.Text = "LISTAR MARCA";
+            this.btnListarMarca.UseVisualStyleBackColor = true;
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnFiltrar.Location = new System.Drawing.Point(21, 12);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(95, 39);
+            this.btnFiltrar.TabIndex = 6;
+            this.btnFiltrar.Text = "FILTRAR";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // txtFiltro
+            // 
+            this.txtFiltro.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtFiltro.Location = new System.Drawing.Point(144, 22);
+            this.txtFiltro.Name = "txtFiltro";
+            this.txtFiltro.Size = new System.Drawing.Size(188, 20);
+            this.txtFiltro.TabIndex = 7;
+            // 
+            // cbFiltro
+            // 
+            this.cbFiltro.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbFiltro.BackColor = System.Drawing.SystemColors.Window;
+            this.cbFiltro.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFiltro.FormattingEnabled = true;
+            this.cbFiltro.Location = new System.Drawing.Point(363, 22);
+            this.cbFiltro.Name = "cbFiltro";
+            this.cbFiltro.Size = new System.Drawing.Size(188, 21);
+            this.cbFiltro.TabIndex = 8;
+            // 
+            // pbArticulo
+            // 
+            this.pbArticulo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbArticulo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pbArticulo.Location = new System.Drawing.Point(537, 112);
+            this.pbArticulo.Name = "pbArticulo";
+            this.pbArticulo.Size = new System.Drawing.Size(236, 224);
+            this.pbArticulo.TabIndex = 9;
+            this.pbArticulo.TabStop = false;
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Location = new System.Drawing.Point(21, 75);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.Size = new System.Drawing.Size(476, 293);
+            this.dgvArticulos.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvArticulos);
+            this.Controls.Add(this.pbArticulo);
+            this.Controls.Add(this.cbFiltro);
+            this.Controls.Add(this.txtFiltro);
+            this.Controls.Add(this.btnFiltrar);
+            this.Controls.Add(this.btnListarMarca);
+            this.Controls.Add(this.btnListarCategoria);
+            this.Controls.Add(this.btnDetalles);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.MaximumSize = new System.Drawing.Size(923, 616);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Catalogo de Artículos";
+            ((System.ComponentModel.ISupportInitialize)(this.pbArticulo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnDetalles;
+        private System.Windows.Forms.Button btnListarCategoria;
+        private System.Windows.Forms.Button btnListarMarca;
+        private System.Windows.Forms.Button btnFiltrar;
+        private System.Windows.Forms.TextBox txtFiltro;
+        private System.Windows.Forms.ComboBox cbFiltro;
+        private System.Windows.Forms.PictureBox pbArticulo;
+        private System.Windows.Forms.DataGridView dgvArticulos;
+    }
+}
+
